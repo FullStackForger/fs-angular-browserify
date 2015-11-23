65ecff48355f8a6cdd37f8ab4d9e1703aa83ad4a
Mark Bevels <Marek@prorustic.home>
2015-11-23 13:59:28 +0000

    angular dep (with bower) & gulp config update

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,23 @@
 var gulp = require('gulp'),
-  connect = require('gulp-connect');
- 
+	connect = require('gulp-connect');
+
 gulp.task('connect', function() {
-  connect.server({
-    root: 'app',
-    livereload: true
-  });
+	connect.server({
+		root: 'public',
+		livereload: true
+	});
 });
  
 gulp.task('html', function () {
-  gulp.src('./app/*.html')
-    .pipe(connect.reload());
+	gulp.src('./public/**/*.html').pipe(connect.reload());
+});
+gulp.task('js', function () {
+	gulp.src('./public/**/*.js').pipe(connect.reload());
 });
  
 gulp.task('watch', function () {
-  gulp.watch(['./app/*.html'], ['html']);
+	gulp.watch(['./public/**/*.html'], ['html']);
+	gulp.watch(['./public/**/*.js'], ['js']);
 });
  
 gulp.task('default', ['connect', 'watch']);
